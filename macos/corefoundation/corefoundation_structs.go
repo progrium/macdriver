0d3ec16583ddaeec1ff26b9e7fae5ee8bf0245bb
Travis Cline <travis.cline@gmail.com>
2023-09-04 16:16:37 -0700

    generate: be a bit more specific about types and functions to skip

--- macos/corefoundation/corefoundation_structs.go
+++ macos/corefoundation/corefoundation_structs.go
@@ -4,28 +4,7 @@ import (
 	"unsafe"
 )
 
-type AllocatorRef = unsafe.Pointer
-type ArrayRef = unsafe.Pointer
-type DataRef = unsafe.Pointer
-type DateRef = unsafe.Pointer
-type DictionaryRef = unsafe.Pointer
-type FileDescriptorRef = unsafe.Pointer
-type MachPortRef = unsafe.Pointer
-type MessagePortRef = unsafe.Pointer
-type NotificationCenterRef = unsafe.Pointer
 type NotificationName = unsafe.Pointer
-type PlugInInstanceRef = unsafe.Pointer
 type PropertyListRef = unsafe.Pointer
-type ReadStreamRef = unsafe.Pointer
-type RunLoopObserverRef = unsafe.Pointer
-type RunLoopRef = unsafe.Pointer
-type RunLoopSourceRef = unsafe.Pointer
-type RunLoopTimerRef = unsafe.Pointer
-type SocketRef = unsafe.Pointer
-type StringRef = unsafe.Pointer
 type TypeRef = unsafe.Pointer
-type URLRef = unsafe.Pointer
-type UUIDRef = unsafe.Pointer
-type UserNotificationRef = unsafe.Pointer
-type WriteStreamRef = unsafe.Pointer
 type XMLExternalID = unsafe.Pointer
